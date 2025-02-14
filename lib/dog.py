920b4635aa02a0ed1f0cbd4a2209ec8beb3d633f
Edwin <edmboche141@gmail.com>
2025-02-14 12:16:03 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,16 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
 
+    def sit(self):
+        print("The dog is sitting.")
+
+# Creating an instance of the Dog class and calling the back method
+fido = Dog()
+fido.bark()
+fido.sit()
     #Instance method definition
-    pass
+
+  
